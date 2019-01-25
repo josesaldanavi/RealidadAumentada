8a15279be9b2f44dc7a902596043bcf5fea36266
Jose Saldaña <josesaldanavi@gmail.com>
2019-01-25 01:12:54 -0500

    Position_Tracking

---
+++ Position_Tracking/Assets/Script/GhostController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GhostController : MonoBehaviour {
+
+    public Transform player;
+    public float sizeRandom;
+    public int puntos;
+    public Text txtPuntos;
+	// Use this for initialization
+	void Start () {
+        cambiarPos();
+        txtPuntos.text = puntos.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnMouseDown()
+    {
+        cambiarPos();
+        SumarPuntos();
+    }
+    void SumarPuntos()
+    {
+        puntos++;
+        txtPuntos.text = puntos.ToString();
+    }
+    
+    void cambiarPos()
+    {
+        transform.position = new Vector3((Random.insideUnitSphere.x * sizeRandom), transform.position.y, (Random.insideUnitSphere.z * sizeRandom));
+        Vector3 posJugador = new Vector3(player.position.x, transform.position.y, player.position.z);
+        transform.LookAt(posJugador);
+    }
+}
